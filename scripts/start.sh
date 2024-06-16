5bac5ff0a27dc444ee8716784b1f51ab5b14ba34
Dackss <thomas.geffroy@outlook.com>
2024-06-16 22:43:11 +0200

    Add colored output and script usage annotations
    The update enhances the script's readability by adding colored output for clarity and overall visual improvement. Function descriptions are added to provide a clearer understanding of their uses. Notably, a new function, print_header, was introduced to print a colorful header at the start of script execution.

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,23 +1,41 @@
 #!/bin/bash
 
-show_menu() {
-    echo "---------------------------------"
-    echo "           MENU PRINCIPAL        "
-    echo "---------------------------------"
-    echo "1. Afficher les informations d'une personne"
-    echo "2. Afficher les enfants d'une personne"
-    echo "3. Afficher les frères et sœurs d'une personne"
-    echo "4. Afficher les ancêtres"
-    echo "5. Exporter en HTML"
-    echo "6. Quitter"
-    echo "---------------------------------"
-    echo -n "Choisissez une option: "
+# This function prints a header for the script.
+# It uses escape codes to color the text green with the option echo -e.
+print_header() {
+    echo -e "\033[32m
+ ____|                   _)  |          __ __|
+ |       _\` |  __ \`__ \   |  |  |   |      |     __|   _ \   _ \   __|
+ __|    (   |  |   |   |  |  |  |   |      |    |      __/   __/ \__ \\
+_|     \__,_| _|  _|  _| _| _| \__, |     _|   _|    \___| \___| ____/
+                               ____/
+\033[0m"
 }
 
+# Same with this function, it uses escape codes to color the text.
+# The menu is displayed.
+show_menu() {
+    echo -e "\033[34m---------------------------------\033[0m"
+    echo -e "\033[33m           MENU PRINCIPAL        \033[0m"
+    echo -e "\033[34m---------------------------------\033[0m"
+    echo -e "\033[31m1. Afficher les informations d'une personne\033[0m"
+    echo -e "\033[31m2. Afficher les enfants d'une personne\033[0m"
+    echo -e "\033[31m3. Afficher les frères et sœurs d'une personne\033[0m"
+    echo -e "\033[31m4. Afficher les ancêtres\033[0m"
+    echo -e "\033[31m5. Exporter en HTML\033[0m"
+    echo -e "\033[31m6. Quitter \033[0m"
+    echo -e "\033[34m---------------------------------\033[0m"
+    echo -e -n "\033[33mChoisissez une option: \033[0m"
+}
+
+
+# This function is used to pause the execution of the script until the user presses a key.
+# The options allow to hide the input and to read only one character.
 pause() {
     read -n 1 -s -r -p "Appuyez sur une touche pour continuer..."
 }
 
+# This function calls the ProjetFinal executable with the show_person_info command and the ID of the person to show.
 show_person_info() {
     echo -n "Entrez l'ID de la personne: "
     read id
@@ -25,6 +43,7 @@ show_person_info() {
     pause
 }
 
+# This function calls the ProjetFinal executable with the get_children command and the ID of the person to show the children.
 get_children() {
     echo -n "Entrez l'ID de la personne: "
     read id
@@ -32,6 +51,7 @@ get_children() {
     pause
 }
 
+# This function calls the ProjetFinal executable with the get_siblings command and the ID of the person to show the siblings.
 get_siblings() {
     echo -n "Entrez l'ID de la personne: "
     read id
@@ -39,6 +59,7 @@ get_siblings() {
     pause
 }
 
+# This function calls the ProjetFinal executable with the get_ancestors command and the ID of the person to show the ancestors.
 get_ancestors() {
     echo -n "Entrez l'ID de la personne: "
     read id
@@ -46,6 +67,7 @@ get_ancestors() {
     pause
 }
 
+# This function calls the ProjetFinal executable with the export_html command and the name of the output file.
 export_html() {
     echo -n "Entrez le nom du fichier de sortie HTML: "
     read filename
@@ -53,9 +75,11 @@ export_html() {
     pause
 }
 
+# The function main of this script use a while loop to show the menu and read the user's choice.
 main() {
     while true; do
         clear
+        print_header
         show_menu
         read choice
         case $choice in
@@ -70,5 +94,5 @@ main() {
     done
 }
 
-# Passer les paramètres à la fonction principale
-main "$@"
+# Basically, this line calls the main function to start the script.
+main
