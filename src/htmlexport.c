74b43a5097c1bca0cc021475c7b002612c01ac7a
QG1212 <qgfy2000@gmail.com>
2024-06-17 10:03:44 +0200

    supp function

--- src/htmlexport.c
+++ src/htmlexport.c
@@ -13,30 +13,6 @@ char *titleHTML(Person * p) {
     return name;
 }
 
-//convertir un arbre en tableau
-int  treeToTableau(Population *p, Person *root, Person **tableau, int index){
-
-    //racine nulle
-    if(root == 0){
-        return index;
-    }
-
-    //Ajout d'une personne
-    if(root == tableau[index]){
-        return index++;
-    }
-
-    //Regarde si la personne a des enfants
-    for(int i=0; i<root->num_children;i++){
-
-        //appel recusif de la fonction treeToTableau lorsqu'un fils a été trouvé
-        if(root->num_children!=0){
-            index = treeToTableau(p,root->children[i], tableau, index);
-
-        }
-    }
-}
-
 void exportTableau_HTML(Population *p, const char *filename){
     FILE *file = fopen(filename, "w");
     if (!file) {
