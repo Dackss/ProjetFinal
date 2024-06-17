61a45588af10028e2e57db5df22fa291024f27f0
Dackss <thomas.geffroy@outlook.com>
2024-06-17 21:24:14 +0200

    Refactor HTML export for family tree generation
    The process of generating HTML for family trees has been refactored. Instead of generating the structure in the export file, it is now built in the htmlexport.c. To align with this, relevant files and file parts have been removed or updated. This change enhances efficiency by consolidating the creation process into the source file.

--- src/htmlexport.c
+++ src/htmlexport.c
@@ -19,77 +19,84 @@ void export_html(Population *population) {
 void update_html(const char *template_filename, const char *output_filename, Person *p, Population *population) {
     FILE *template_file = fopen(template_filename, "r");
     if (!template_file) {
-        printf("Error");
+        printf("Erreur");
         fclose(template_file);
         return;
     }
 
     FILE *output_file = fopen(output_filename, "w");
     if (!output_file) {
-        printf("Error");
+        printf("Erreur");
         fclose(template_file);
         return;
     }
 
-    char line[1024];
-    while (fgets(line, sizeof(line), template_file)) {
-        if (strstr(line, "<!-- Génération 1 -->")) {
-            fprintf(output_file, "            <div class='generation'>\n");
-            fprintf(output_file, "                <div class='member'>\n");
-            fprintf(output_file, "                    <img src='img.jpg' alt='%s %s'>\n", p->firstname, p->lastname);
-            fprintf(output_file, "                    <p>%s %s</p>\n", p->firstname, p->lastname);
-            fprintf(output_file, "                    <div class='connection'></div>\n");
-            fprintf(output_file, "                </div>\n");
-            fprintf(output_file, "            </div>\n");
+    fprintf(output_file, "<!DOCTYPE html>\n");
+    fprintf(output_file, "<html lang=\"fr\">\n");
+    fprintf(output_file, "<head>\n");
+    fprintf(output_file, "    <meta charset=\"UTF-8\">\n");
+    fprintf(output_file, "    <link rel=\"stylesheet\" href=\"genealogie.css\">\n");
+    fprintf(output_file, "    <title>Arbre Généalogique</title>\n");
+    fprintf(output_file, "</head>\n");
+    fprintf(output_file, "<body>\n");
+    fprintf(output_file, "<header>\n");
+    fprintf(output_file, "    <div class=\"header-left\">\n");
+    fprintf(output_file, "        <h1>Nom de l'Entreprise</h1>\n");
+    fprintf(output_file, "    </div>\n");
+    fprintf(output_file, "    <div class=\"header-right\">\n");
+    fprintf(output_file, "        <h1>Nom de la Famille</h1>\n");
+    fprintf(output_file, "    </div>\n");
+    fprintf(output_file, "</header>\n");
+    fprintf(output_file, "<main>\n");
+    fprintf(output_file, "    <div class=\"tree\">\n");
+    fprintf(output_file, "        <ul>\n");
+    fprintf(output_file, "            <li>\n");
+    fprintf(output_file, "                <a href=\"#\"><img src=\"img.jpg\" alt=\"%s %s\">%s %s</a>\n", p->firstname, p->lastname, p->firstname, p->lastname);
+    fprintf(output_file, "                <ul>\n");
 
-        } else if (strstr(line, "<!-- Génération 2 -->")) {
-            fprintf(output_file, "            <div class='generation'>\n");
-            if (p->p_father) {
-                fprintf(output_file, "                <div class='member'>\n");
-                fprintf(output_file, "                    <img src='img.jpg' alt='%s %s'>\n", p->p_father->firstname, p->p_father->lastname);
-                fprintf(output_file, "                    <p>%s %s</p>\n", p->p_father->firstname, p->p_father->lastname);
-                fprintf(output_file, "                </div>\n");
-            }
-            if (p->p_mother) {
-                fprintf(output_file, "                <div class='member'>\n");
-                fprintf(output_file, "                    <img src='img.jpg' alt='%s %s'>\n", p->p_mother->firstname, p->p_mother->lastname);
-                fprintf(output_file, "                    <p>%s %s</p>\n", p->p_mother->firstname, p->p_mother->lastname);
-                fprintf(output_file, "                </div>\n");
-            }
-            fprintf(output_file, "            </div>\n");
-
-        } else if (strstr(line, "<!-- Génération 3 -->")) {
-            fprintf(output_file, "            <div class='generation'>\n");
-            if (p->p_father && p->p_father->p_father) {
-                fprintf(output_file, "                <div class='member'>\n");
-                fprintf(output_file, "                    <img src='img.jpg' alt='%s %s'>\n", p->p_father->p_father->firstname, p->p_father->p_father->lastname);
-                fprintf(output_file, "                    <p>%s %s</p>\n", p->p_father->p_father->firstname, p->p_father->p_father->lastname);
-                fprintf(output_file, "                </div>\n");
-            }
-            if (p->p_father && p->p_father->p_mother) {
-                fprintf(output_file, "                <div class='member'>\n");
-                fprintf(output_file, "                    <img src='img.jpg' alt='%s %s'>\n", p->p_father->p_mother->firstname, p->p_father->p_mother->lastname);
-                fprintf(output_file, "                    <p>%s %s</p>\n", p->p_father->p_mother->firstname, p->p_father->p_mother->lastname);
-                fprintf(output_file, "                </div>\n");
-            }
-            if (p->p_mother && p->p_mother->p_father) {
-                fprintf(output_file, "                <div class='member'>\n");
-                fprintf(output_file, "                    <img src='img.jpg' alt='%s %s'>\n", p->p_mother->p_father->firstname, p->p_mother->p_father->lastname);
-                fprintf(output_file, "                    <p>%s %s</p>\n", p->p_mother->p_father->firstname, p->p_mother->p_father->lastname);
-                fprintf(output_file, "                </div>\n");
-            }
-            if (p->p_mother && p->p_mother->p_mother) {
-                fprintf(output_file, "                <div class='member'>\n");
-                fprintf(output_file, "                    <img src='img.jpg' alt='%s %s'>\n", p->p_mother->p_mother->firstname, p->p_mother->p_mother->lastname);
-                fprintf(output_file, "                    <p>%s %s</p>\n", p->p_mother->p_mother->firstname, p->p_mother->p_mother->lastname);
-                fprintf(output_file, "                </div>\n");
-            }
-            fprintf(output_file, "            </div>\n");
-
+    if (p->p_father) {
+        fprintf(output_file, "                    <li>\n");
+        fprintf(output_file, "                        <a href=\"#\"><img src=\"img.jpg\" alt=\"%s %s\">%s %s</a>\n", p->p_father->firstname, p->p_father->lastname, p->p_father->firstname, p->p_father->lastname);
+        fprintf(output_file, "                        <ul>\n");
+        if (p->p_father->p_father) {
+            fprintf(output_file, "                            <li><a href=\"#\"><img src=\"img.jpg\" alt=\"%s %s\">%s %s</a></li>\n", p->p_father->p_father->firstname, p->p_father->p_father->lastname, p->p_father->p_father->firstname, p->p_father->p_father->lastname);
         } else {
-            fprintf(output_file, "%s", line);
+            fprintf(output_file, "                            <li><a href=\"#\"><img src=\"img.jpg\" alt=\"Inconnu\">Inconnu</a></li>\n");
         }
+        if (p->p_father->p_mother) {
+            fprintf(output_file, "                            <li><a href=\"#\"><img src=\"img.jpg\" alt=\"%s %s\">%s %s</a></li>\n", p->p_father->p_mother->firstname, p->p_father->p_mother->lastname, p->p_father->p_mother->firstname, p->p_father->p_mother->lastname);
+        } else {
+            fprintf(output_file, "                            <li><a href=\"#\"><img src=\"img.jpg\" alt=\"Inconnu\">Inconnu</a></li>\n");
+        }
+        fprintf(output_file, "                        </ul>\n");
+        fprintf(output_file, "                    </li>\n");
     }
+
+    if (p->p_mother) {
+        fprintf(output_file, "                    <li>\n");
+        fprintf(output_file, "                        <a href=\"#\"><img src=\"img.jpg\" alt=\"%s %s\">%s %s</a>\n", p->p_mother->firstname, p->p_mother->lastname, p->p_mother->firstname, p->p_mother->lastname);
+        fprintf(output_file, "                        <ul>\n");
+        if (p->p_mother->p_father) {
+            fprintf(output_file, "                            <li><a href=\"#\"><img src=\"IMG.jpg\" alt=\"%s %s\">%s %s</a></li>\n", p->p_mother->p_father->firstname, p->p_mother->p_father->lastname, p->p_mother->p_father->firstname, p->p_mother->p_father->lastname);
+        } else {
+            fprintf(output_file, "                            <li><a href=\"#\"><img src=\"img.jpg\" alt=\"Inconnu\">Inconnu</a></li>\n");
+        }
+        if (p->p_mother->p_mother) {
+            fprintf(output_file, "                            <li><a href=\"#\"><img src=\"img.jpg\" alt=\"%s %s\">%s %s</a></li>\n", p->p_mother->p_mother->firstname, p->p_mother->p_mother->lastname, p->p_mother->p_mother->firstname, p->p_mother->p_mother->lastname);
+        } else {
+            fprintf(output_file, "                            <li><a href=\"#\"><img src=\"img.jpg\" alt=\"Inconnu\">Inconnu</a></li>\n");
+        }
+        fprintf(output_file, "                        </ul>\n");
+        fprintf(output_file, "                    </li>\n");
+    }
+
+    fprintf(output_file, "                </ul>\n");
+    fprintf(output_file, "            </li>\n");
+    fprintf(output_file, "        </ul>\n");
+    fprintf(output_file, "    </div>\n");
+    fprintf(output_file, "</main>\n");
+    fprintf(output_file, "</body>\n");
+    fprintf(output_file, "</html>\n");
 
     fclose(template_file);
     fclose(output_file);
